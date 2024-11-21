09cdcc377f89695bb5cf0b6fd819042553000296
Juan Milkah <juanmilkah@gmail.com>
2024-11-21 10:23:14 +0300

    changed project file tree arrangement
    removed redundant clinews directory

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 use std::error::Error;
 
-use clinews::{get_articles, Articles,};
+use clinewsapp::{get_articles, Articles};
 use colour::{dark_blue, yellow};
 use dotenv::dotenv;
 
-
-fn render_articles(articles: &Articles){
-    for a in &articles.articles{
-        dark_blue!("> {}\n\n",a.title);
-        yellow!("> {}\n\n",a.url);
+fn render_articles(articles: &Articles) {
+    for a in &articles.articles {
+        dark_blue!("> {}\n\n", a.title);
+        yellow!("> {}\n\n", a.url);
     }
 }
 
-fn main()-> Result<(),Box<dyn Error>> {
-    
+fn main() -> Result<(), Box<dyn Error>> {
     dotenv()?;
 
+    let apikey = std::env::var("API_KEY")?;
 
-  let apikey= std::env::var("API_KEY")?;
-    
     let url = "https://newsapi.org/v2/everything?q=apple&from=2024-10-15&to=2024-10-15&sortBy=popularity&apiKey=";
-    let url = format!("{}{}" , url, apikey);
+    let url = format!("{}{}", url, apikey);
     let articles = get_articles(&url)?;
     render_articles(&articles);
-   Ok(())
+    Ok(())
 }
-
